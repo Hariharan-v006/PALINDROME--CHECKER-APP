bafd69c9f10402ce7d9891c3f28a8479df8b9055
Hariharan-v006 <hari.work2006@gmail.com>
2026-03-02 14:21:52 +0530

    char array checker code updated

--- App/src/char_array_checker.java
+++ App/src/char_array_checker.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class char_array_checker {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the text: ");
+        String text = input.nextLine();
+        text = text.toLowerCase(Locale.ROOT);
+        boolean flag = true;
+        char[] test_string = text.toCharArray();
+        int start = 0;
+        int end = text.length()-1;
+        while(start<end){
+            if(test_string[start] != test_string[end]){
+                flag = false;
+                break;
+            }
+            start++; end--;
+        }
+        System.out.println((flag)?"It's a palindrome":"Not a palindrome");
+    }
 }
