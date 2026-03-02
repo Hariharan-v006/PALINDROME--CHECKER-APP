923cede9134e2fb0474e08292c4ce1d5caefe00a
Hariharan-v006 <hari.work2006@gmail.com>
2026-03-02 14:34:17 +0530

    stack based palindrome checker

---
+++ App/src/stack_based.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class stack_based {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the text: ");
+        String text = input.nextLine();
+        text = text.toLowerCase(Locale.ROOT);
+        boolean flag = true;
+        Stack<Character> stack = new Stack<>();
+        for( char c: text.toCharArray()){
+            stack.push(c);
+        }
+        for(char c: text.toCharArray()){
+            if(!stack.pop().equals(c)){
+                flag = false;
+                break;
+            }
+        }
+        System.out.println((flag)?"It's a palindrome":"Not a palindrome");
+    }
+}
