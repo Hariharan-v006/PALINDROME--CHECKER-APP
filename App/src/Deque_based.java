d1a27825686627f0476a27352def33ad0f199d71
Hariharan-v006 <hari.work2006@gmail.com>
2026-03-03 12:04:07 +0530

    deque based checker

---
+++ App/src/Deque_based.java
@@ -0,0 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Deque_based {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the text: ");
+        String text = input.nextLine();
+        text = text.toLowerCase(Locale.ROOT);
+        boolean flag = true;
+        Deque<Character> deque = new ArrayDeque<>();
+        for(char c: text.toCharArray()){
+            deque.add(c);
+        }
+        while(deque.size()>1){
+            if(!deque.removeFirst().equals(deque.removeLast())){
+                flag = false;
+                break;
+            }
+        }
+        System.out.println((flag)?"It's a palindrome":"Not a palindrome");
+    }
+}
